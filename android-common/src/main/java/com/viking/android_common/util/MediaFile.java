9b0c66d25ac4f37c3294998c01b2ba07f339681a
viking den <vikingden@live.com>
2016-08-09 22:39:13 +0800

    add info

--- android-common/src/main/java/com/viking/android_common/util/MediaFile.java
+++ android-common/src/main/java/com/viking/android_common/util/MediaFile.java
@@ -1,13 +1,27 @@
+/*
+ * Copyright (C) 2016 Viking Den <vikingden@live.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.viking.android_common.util;
-
-import android.mtp.MtpConstants;
 
 import java.util.HashMap;
 import java.util.Locale;
 
 /**
- * MediaScanner helper class.
- *
+ * Utility class for media file judge, base on android.media.MediaFile.java
+ * author : Viking Den <vikingden@live.com>
+ * time   : 2016/8/9 22:38
  */
 public class MediaFile {
 
